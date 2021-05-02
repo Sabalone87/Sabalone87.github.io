fc4f08efd0c24094c8d2683123bb42cc9fa20d4f
charlie <b06501012@gmail.com>
2021-05-03 00:22:14 +0800

    update

--- myscript.js
+++ myscript.js
@@ -20,11 +20,89 @@ $(".fa-line").on("click", function () {
 //project
 $("#htmlcss").on("click", function () {
   $("#nav-tag").text("HTML / CSS / JAVASCRIPT");
-  console.log("hi");
+  $("#project1").css("background-image", "url(./img/musicPlayer.png)");
+  $("#project2").css("background-image", "url(./img/musicPlayer.png)");
+  $("#project3").css("background-image", "url(./img/comming.jpg)");
+  $("#project1-text").text("MusicPlayer");
+  $("#project2-text").text("MyWeb");
+  $("#project3-text").text("Comming soon");
+  $("#project1-link").attr({
+    href: "https://github.com/Sabalone87/musicPlayer",
+    target: "_blank",
+  });
+  $("#project2-link").attr({ href: "./index.html", target: "_blank" });
+  $("#project3-link").attr({ href: "#project", target: "_self" });
 });
 $("#react").on("click", function () {
   $("#nav-tag").text("REACT.JS");
+  $("#project1").css("background-image", "url(./img/todo.png)");
+  $("#project2").css("background-image", "url(./img/comming.jpg)");
+  $("#project3").css("background-image", "url(./img/comming.jpg)");
+  $("#project1-text").text("TodoList");
+  $("#project2-text").text("Comming soon");
+  $("#project3-text").text("Comming soon");
+  $("#project1-link").attr({
+    href: "https://github.com/Sabalone87/TodoList",
+    target: "_blank",
+  });
+  $("#project2-link").attr({ href: "#project", target: "_self" });
+  $("#project3-link").attr({ href: "#project", target: "_self" });
 });
 $("#node").on("click", function () {
   $("#nav-tag").text("NODE.JS");
+  $("#project1").css("background-image", "url(./img/comming.jpg)");
+  $("#project2").css("background-image", "url(./img/comming.jpg)");
+  $("#project3").css("background-image", "url(./img/comming.jpg)");
+  $("#project1-text").text("Comming soon");
+  $("#project2-text").text("Comming soon");
+  $("#project3-text").text("Comming soon");
+  $("#project1-link").attr({ href: "#project", target: "_self" });
+  $("#project2-link").attr({ href: "#project", target: "_self" });
+  $("#project3-link").attr({ href: "#project", target: "_self" });
 });
+
+$("#project1").hover(
+  function () {
+    $("#project1-text").animate({ opacity: 1 }, 500);
+    $("#project1-btn").animate({ opacity: 1 }, 500);
+    $("#project1-cover").animate({ opacity: 0.3 }, 500);
+  },
+  function () {
+    $("#project1-text").animate({ opacity: 0 }, 100);
+    $("#project1-btn").animate({ opacity: 0 }, 100);
+    $("#project1-cover").animate({ opacity: 0 }, 100);
+  }
+);
+$("#project2").hover(
+  function () {
+    $("#project2-text").animate({ opacity: 1 }, 100);
+    $("#project2-btn").animate({ opacity: 1 }, 100);
+    $("#project2-cover").animate({ opacity: 0.3 }, 100);
+  },
+  function () {
+    $("#project2-text").animate({ opacity: 0 }, 100);
+    $("#project2-btn").animate({ opacity: 0 }, 100);
+    $("#project2-cover").animate({ opacity: 0 }, 100);
+  }
+);
+$("#project3").hover(
+  function () {
+    $("#project3-text").animate({ opacity: 1 }, 500);
+    $("#project3-btn").animate({ opacity: 1 }, 500);
+    $("#project3-cover").animate({ opacity: 0.3 }, 500);
+  },
+  function () {
+    $("#project3-text").animate({ opacity: 0 }, 100);
+    $("#project3-btn").animate({ opacity: 0 }, 100);
+    $("#project3-cover").animate({ opacity: 0 }, 100);
+  }
+);
+
+//contact
+
+let form = $("#form"),
+  name = $("#username"),
+  email = $("#useremail"),
+  subject = $("#usersubject"),
+  msg = $("#usermsg"),
+  submit = $("#submit");
